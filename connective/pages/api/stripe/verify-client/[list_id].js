53e3800f9702a6ef4a8c09a6f26f05bdc3dffbf3
lovely0806 <star.software.guy@gmail.com>
2022-10-26 07:32:24 +0545

    DB changes

--- connective/pages/api/stripe/verify-client/[list_id].js
+++ connective/pages/api/stripe/verify-client/[list_id].js
@@ -1,43 +1,50 @@
 import { withIronSession } from "next-iron-session";
-
 const mysql = require("mysql2");
-
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(req, res) {
   try {
     if (req.method == "GET") {
       const listID = req.query.list_id;
+      console.log(listID);
       // fetch connected account ID from the database
       const connection = mysql.createConnection(process.env.DATABASE_URL);
       var [result, fields, err] = await connection
         .promise()
         .query(
-          `SELECT Lists.price, User.stripeID FROM Lists WHERE id='${listID}' INNER JOIN User ON Lists.creator = User.id`
+          `SELECT Lists.price, Users.stripeID FROM Lists INNER JOIN Users ON Lists.creator = Users.id WHERE Lists.id="${listID}"`
         );
-      const listInformation = result[0];
-      //
+      console.log(result);
       connection.close();
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: listInformation.price,
-        currency: "usd",
-        automatic_payment_methods: {
-          enabled: true,
-        },
-        application_fee_amount: process.env.feePercentage * listInformation.price,
-        transfer_data: {
-          destination: listInformation.stripeID,
-        },
-      });
+      if (result.length > 0) {
+        const listInformation = result[0];
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: listInformation.price,
+          currency: "usd",
+          automatic_payment_methods: {
+            enabled: true,
+          },
+          application_fee_amount:
+            process.env.feePercentage * listInformation.price,
+          transfer_data: {
+            destination: listInformation.stripeID,
+          },
+        });
 
-      return res
-        .status(200)
-        .json({ client_secret: paymentIntent.client_secret, error: false });
+        return res
+          .status(200)
+          .json({ client_secret: paymentIntent.client_secret, error: false });
+      } else {
+        res.status(400).json({ client_secret: null, error: "Wrong List Id" });
+      }
     } else {
-      res.status(400).json({ client_secret: null, error: 'Only GET method allowed' });
+      res
+        .status(400)
+        .json({ client_secret: null, error: "Only GET method allowed" });
     }
-  } catch {
-    res.status(500).json({ error: 'Server Error' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server Error" });
   }
 }
 
