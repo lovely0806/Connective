94591f9412e793436019c1f8f96166296d0ad1a0
lovely0806 <star.software.guy@gmail.com>
2022-10-27 19:36:35 +0545

    Final Commit

--- connective/pages/api/stripe/connect-seller/index.js
+++ connective/pages/api/stripe/connect-seller/index.js
@@ -1,19 +1,17 @@
 import { withIronSession } from "next-iron-session";
 
 const mysql = require("mysql2");
-const client = require('@sendgrid/mail');
+const client = require("@sendgrid/mail");
 
-const stripe = require("stripe")(
-  process.env.STRIPE_SECRET_KEY
-);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(req, res) {
   try {
     if (req.method === "POST") {
       const connection = mysql.createConnection(process.env.DATABASE_URL);
-      let user = req.session.get().user
-      if(typeof(user) == "undefined") {
-          return res.status(500).json({success: false, error: "Not signed in"})
+      let user = req.session.get().user;
+      if (typeof user == "undefined") {
+        return res.status(500).json({ success: false, error: "Not signed in" });
       }
       var [result, fields, err] = await connection
         .promise()
@@ -28,26 +26,25 @@ export async function handler(req, res) {
           type: "account_onboarding",
         });
         // also might update the db with userOnbarded to true
-        // give this link to the user via email for other things for connecting his/her bank information with stripe. 
+        // give this link to the user via email for other things for connecting his/her bank information with stripe.
         // email
         client.setApiKey(process.env.SEND_GRID_API_KEY);
         client
           .send({
             from: process.env.EMAIL_USER,
             to: result[0].email,
-            subject: "Bank Verification",
-            html: `<a link="${accountLink.url}">Stripe Url Click Here</a>`
+            subject: "Verify your payment details for Connective",
+            html: `<h1>Welcome to Connective</h1><br/> <div>Please <a href=${accountLink.url} style="color: '#00acee'">click here</a> to configure your payment details.</div>`,
           })
           .then(() => {
             return res.status(200).json({ success: true });
           })
           .catch((error) => {
-            console.log(error)
+            console.log(error);
             return res
               .status(500)
               .json({ error: "Email doesnot work", success: false });
           });
-
       } else {
         return res.json({ error: "User not found", success: false });
       }
@@ -57,7 +54,7 @@ export async function handler(req, res) {
   } catch {
     return res.json({ error: "Server error", success: false });
   }
-};
+}
 
 export default withIronSession(handler, {
   password: process.env.APPLICATION_SECRET,
--- connective/pages/api/stripe/test/index.js
+++ connective/pages/api/stripe/test/index.js
@@ -1,23 +1,31 @@
 const client = require("@sendgrid/mail");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
+    const stripe_account = await stripe.accounts.create({ type: "express" });
+    const accountLink = await stripe.accountLinks.create({
+      account: stripe_account.id,
+      refresh_url: process.env.refreshURL,
+      return_url: process.env.returnURL,
+      type: "account_onboarding",
+    });
     client.setApiKey(process.env.SEND_GRID_API_KEY);
     client
       .send({
         from: process.env.EMAIL_USER,
-        to: "aweb5031@gmail.com",
-        subject: "Bank Verification",
-        html: "<h1>HELLO WORLD</h1>"
+        to: "abhinavbhattarai09@gmail.com",
+        subject: "Verify your payment details for Connective",
+        html: `<h1>Welcome to Connective</h1><br/> <div>Please <a href=${accountLink.url} style="color: '#00acee'">click here</a> to configure your payment details.</div>`,
       })
       .then(() => {
         return res.status(200).json({ success: true });
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
         return res
           .status(500)
           .json({ error: "Email doesnot work", success: false });
       });
   }
-};
+}
