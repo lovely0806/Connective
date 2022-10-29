7ee43de6928dc26328c0838b7962fa8b526a764f
lovely0806 <star.software.guy@gmail.com>
2022-10-29 09:07:22 -0400

    Sign in enhancements

--- connective/components/onboarding-sidebar/index.js
+++ connective/components/onboarding-sidebar/index.js
@@ -1,10 +1,14 @@
+import Link from "next/link"
+
 const OnboardingSidebar = () => {
     return (
-        <div className="w-[15vw] h-fill rounded-r-lg bg-[#0F172A] flex flex-col">
-            <div className="flex flex-row my-5 mx-auto">
-                <img className="my-auto w-[3vw]" src="../assets/logo-icon-white.png"></img>
-                <img className="mt-1 w-[10vw] object-scale-down" src="../assets/logo-text-white.png"></img>
-            </div>
+        <div className="w-[25vw] h-fill rounded-r-lg bg-[#0F172A] flex flex-col">
+            <Link href="https://www.connective-app.xyz">
+                <div className="flex flex-row my-5 mx-auto cursor-pointer">
+                    <img className="my-auto w-[3vw]" src="../assets/logo-icon-white.png"></img>
+                    <img className="mt-1 w-[10vw] object-scale-down" src="../assets/logo-text-white.png"></img>
+                </div>
+            </Link>
         </div>
     )
 }
