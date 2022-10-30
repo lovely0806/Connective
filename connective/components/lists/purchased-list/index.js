1e7ee388fcbc779f46116e1f57110b8f6c3200d1
lovely0806 <star.software.guy@gmail.com>
2022-10-29 20:38:32 -0400

    Download file

--- connective/components/lists/purchased-list/index.js
+++ connective/components/lists/purchased-list/index.js
@@ -1,9 +1,12 @@
 import ButtonDark from "../../button-dark"
 import ButtonLight from "../../button-light"
 import ButtonGreen from "../../button-green"
+import { useRouter } from "next/router"
 import Image from "next/image"
 
 const ListRow = ({item}) => {
+    const router = useRouter()
+
     return (
         <div className="bg-white rounded-xl shadow-lg flex flex-row gap-5 p-5">
             <div className="rounded-xl object-cover h-48 w-80 relative overflow-hidden">
@@ -21,7 +24,7 @@ const ListRow = ({item}) => {
                 </div>
             </div>
             <div className="flex flex-col ml-auto my-auto gap-5">
-                <ButtonGreen text="Download File" className="mt-0 mb-0 w-full"></ButtonGreen>
+                <ButtonGreen text="Download File" className="mt-0 mb-0 w-full" onClick={()=>{router.push(item.url)}}></ButtonGreen>
                 <ButtonDark text="Edit" className="w-full mt-0 mb-0"></ButtonDark>
                 <ButtonLight text="Unpublish" className="w-full mt-0 mb-0"></ButtonLight>
             </div>
