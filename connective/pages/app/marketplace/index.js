cbf41883dd59e366d6ca89273343e3a1152bce88
lovely0806 <star.software.guy@gmail.com>
2022-10-31 18:02:05 -0400

    Added search

--- connective/pages/app/marketplace/index.js
+++ connective/pages/app/marketplace/index.js
@@ -12,6 +12,7 @@ export default function Dashboard({user}) {
     const [filteredLists, setFilteredLists] = useState([])
     const [sort, setSort] = useState("")
     const [filter, setFilter] = useState(0)
+    const [search, setSearch] = useState("")
 
     const sortOptions = [
         {value: "Buyers", label: "Buyers"},
@@ -53,6 +54,8 @@ export default function Dashboard({user}) {
             tempFiltered = temp
         }
 
+        tempFiltered = tempFiltered.filter(i => {return i.title.toLowerCase().includes(search.toLowerCase()) || i.description.toLowerCase().includes(search.toLowerCase()) || i.username.toLowerCase().includes(search.toLowerCase())})
+
         if(sort == "Buyers") {
             tempFiltered = tempFiltered.sort((a,b) => {
                 return b.buyers - a.buyers
@@ -66,13 +69,13 @@ export default function Dashboard({user}) {
         }
 
         setFilteredLists([...tempFiltered])
-    }, [filter, sort, lists])
+    }, [filter, sort, lists, search])
 
     return (
         <Layout title="Marketplace">
             <div className="mx-20 h-screen">
                 <div className="flex flex-row w-full mb-20 gap-10">
-                    <input placeholder="Search for lists here" style={{background: "white url(/assets/search.svg) no-repeat 5px 5px"}} className="h-fit w-full mr-32 outline-none pl-10 px-5 py-2 border border-black/20 rounded-md focus:outline-blue-200 transition-all hover:outline hover:outline-blue-300"></input>
+                    <input onChange={(e)=>{setSearch(e.target.value)}} placeholder="Search for lists here" style={{background: "white url(/assets/search.svg) no-repeat 5px 5px"}} className="h-fit w-full mr-32 outline-none pl-10 px-5 py-2 border border-black/20 rounded-md focus:outline-blue-200 transition-all hover:outline hover:outline-blue-300"></input>
                     <Select options={categoryOptions} defaultValue={categoryOptions[0]} onChange={updateFilter} isMulti={false} placeholder="Categories" className="w-96"></Select>
                     <Select options={sortOptions} onChange={updateSort} placeholder="Sort" className="w-96"></Select>
                 </div>
