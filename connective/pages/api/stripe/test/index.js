bf4a88974eca8ba10cc9548ef44f603f745c6bba
lovely0806 <star.software.guy@gmail.com>
2022-10-30 21:35:20 -0400

    Remove test dir

--- connective/pages/api/stripe/test/index.js
+++
@@ -1,9 +0,0 @@
-const client = require("@sendgrid/mail");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-export default async function handler(req, res) {
-  if (req.method === "GET") {
-    const stripe_account = await stripe.accounts.create({ type: "express" });
-    console.log(stripe_account.id);
-  }
-}
