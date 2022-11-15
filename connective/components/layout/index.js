2243b9b1e3e7278bd68fd005c49b0e9b02907828
lovely0806 <star.software.guy@gmail.com>
2022-11-15 15:58:41 -0500

    Revert "Messages"

--- connective/components/layout/index.js
+++ connective/components/layout/index.js
@@ -1,10 +1,10 @@
 import Sidebar from "../sidebar";
 
-const Layout = ({ title, scroll=true, children }) => {
+const Layout = ({ title, children }) => {
   return (
-    <main className={`flex flex-row ${scroll ? "min-h-screen" : "h-screen max-h-screen"} min-w-screen font-[Montserrat]`}>
-      <Sidebar></Sidebar>
-      <div className={`w-screen h-screen ${scroll ? "overflow-y-scroll" : "h-full max-h-screen"} flex flex-col bg-[#F5F5F5] relative`}>
+    <main className="flex flex-row min-h-screen min-w-screen font-[Montserrat]">
+      <Sidebar />
+      <div className="w-screen h-screen overflow-y-scroll flex flex-col bg-[#F5F5F5] relative">
         <p className="font-bold text-3xl leading-[29px] text-[#0D1011] mt-[65px] ml-[64px]">
           {title}
         </p>
