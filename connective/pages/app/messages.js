0c2400615d55d0e3da6dfeb60aae56c1a674d072
lovely0806 <star.software.guy@gmail.com>
2022-11-15 15:48:52 -0500

    Updated ui. Added search

--- connective/pages/app/messages.js
+++ connective/pages/app/messages.js
@@ -8,13 +8,13 @@ import Select from "react-select"
 const Message = ({text, sent}) => {
     if(sent) {
         return (
-            <div className="ml-auto bg-blue-100 w-2/5 p-2 rounded-lg shadow-md">
+            <div className="ml-auto bg-blue-100 w-3/5 p-2 rounded-lg shadow-md">
                 <p>{text}</p>
             </div>
         )
     } else {
         return (
-            <div className="bg-slate-100 w-2/5 p-2 rounded-lg shadow">
+            <div className="bg-slate-100 w-3/5 p-2 rounded-lg shadow">
                 <p>{text}</p>
             </div>
         )
@@ -22,9 +22,18 @@ const Message = ({text, sent}) => {
 }
 
 const Conversations = ({selectedUser, setSelectedUser, conversations}) => {
+    const [filter, setFilter] = useState("")
+    const [filteredConversations, setFilteredConversations] = useState(conversations)
+
+    useEffect(() => {
+      setFilteredConversations(conversations.filter(a => a.username.includes(filter) || a.email.includes(filter)))
+    }, [filter])
+
     return (
-        <div className="flex flex-col w-1/5 overflow-y-scroll">
-            {conversations.map((item, index) => {
+        <div className="flex flex-col w-1/5 overflow-y-scroll bg-black/5">
+            <input placeholder="Search..." onChange={(e)=>{setFilter(e.target.value)}} className="outline-none pl-[32px] pr-[14px] text-sm py-2 rounded-full m-5 shadow-lg focus:outline-blue-200 transition-all hover:outline hover:outline-blue-300"></input>
+
+            {filteredConversations.map((item, index) => {
                 return (
                     <div onClick={()=>{setSelectedUser(item)}} className={`flex flex-row p-2 cursor-pointer border-b border-slate-200 ${selectedUser?.id == item.id ? "bg-white" : "bg-slate-100"} hover:bg-slate-100/50 transition-all`}>
                         <img src={item.logo ? item.logo : `https://avatars.dicebear.com/api/micah/${item.id}.svg`} className="w-12 h-12 bg-white rounded-full shadow-lg"/>
@@ -33,6 +42,7 @@ const Conversations = ({selectedUser, setSelectedUser, conversations}) => {
                 )
             })}
 
+            {/*
             <div
                 className="flex flex-row cursor-pointer text-white rounded-lg bg-[#061A40] hover:bg-[#0a2352] transition-all py-3 m-5"
                 onClick={() => setSelectedUser(null)}
@@ -41,6 +51,7 @@ const Conversations = ({selectedUser, setSelectedUser, conversations}) => {
                 New Chat
             </p>
             </div>
+            */}
         </div>
     )
 }
@@ -97,18 +108,7 @@ const Chat = ({users, selectedUser, setSelectedUser, user, conversations, getCon
     return (
         <div className="flex flex-col h-full w-4/5 rounded-r-lg">
             <div className="flex flex-row w-full p-2">
-                {selectedUser != null ? (
-                    <p className="font-medium text-xl w-full text-center mt-2 pb-2 border-b-2 border-slate-100">{selectedUser.username + " (" + selectedUser.email + ")"}</p>
-                ) : (
-                    <Select
-                        className="font-[Poppins] w-full"
-                        onChange={(e) => {
-                        setSelectedUser(users.filter(a => a.id == e.value)[0]);
-                        }}
-                        options={userOptions}
-                        placeholder="Search for a User"
-                    ></Select>
-                )}
+                <p className="font-medium text-lg w-full mt-2 pb-2 border-b-2 border-slate-100">{selectedUser.username + " (" + selectedUser.email + ")"}</p>
             </div>
             <div id="messages-container" className="h-full overflow-y-scroll p-5 flex flex-col gap-10">
                 {messages.map((item, index) => {
@@ -127,10 +127,39 @@ const Chat = ({users, selectedUser, setSelectedUser, user, conversations, getCon
     )
 }
 
+const UserDetails = ({selectedUser}) => {
+  return (
+    <div className="w-1/5 h-full px-2">
+      {selectedUser && (
+        <>
+          <img src={selectedUser.logo ? selectedUser.logo : `https://avatars.dicebear.com/api/micah/${selectedUser.id}.svg`} className="w-[40%] mx-auto bg-white rounded-full shadow-lg"/>
+          <p className="font-bold text-lg text-center mt-5">{selectedUser.username}</p>
+
+          <p className="text-sm font-bold mt-10">Contact Details:</p>
+          <div className="flex flex-row gap-2 my-5">
+            <div className="bg-black/5 rounded-full p-[7px] w-8 h-8 shadow-lg">
+              <img src="/assets/email.png"></img>
+            </div>
+            <p className="text-sm my-auto break-all">{selectedUser.email}</p>
+          </div>
+          <div className="flex flex-row gap-2">
+            <div className="bg-black/5 rounded-full p-[7px] w-8 h-8 shadow-lg">
+              <img src="/assets/location.png"></img>
+            </div>
+            <p className="text-sm my-auto break-all">{selectedUser.location}</p>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+
 export default function Messages({ user }) {
     const [users, setUsers] = useState([])
     const [selectedUser, setSelectedUser] = useState()
     const [conversations, setConversations] = useState([])
+
+    console.log(selectedUser)
 
     const getUsers = async() => {
         const {data} = await axios.get("/api/profiles")
@@ -161,9 +190,10 @@ export default function Messages({ user }) {
 
     return (
       <Layout title="Messages" scroll={false}>
-        <div className="bg-white shadow-lg rounded-lg h-full mt-5 flex flex-row">
+        <div className="bg-white h-full overflow-clip mt-5 flex flex-row">
             <Conversations conversations={conversations} setSelectedUser={setSelectedUser} selectedUser={selectedUser}></Conversations>
             <Chat user={user} users={users} selectedUser={selectedUser} setSelectedUser={setSelectedUser} conversations={conversations} getConversations={getConversations}></Chat>
+            <UserDetails selectedUser={selectedUser}></UserDetails>
         </div>
       </Layout>
     );
