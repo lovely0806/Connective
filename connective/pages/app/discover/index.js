d3d9504b5661d3f6602c9e23962d26abee6f00d2
lovely0806 <star.software.guy@gmail.com>
2022-12-20 14:41:57 -0500

    Added recache to discover page

--- connective/pages/app/discover/index.js
+++ connective/pages/app/discover/index.js
@@ -16,7 +16,9 @@ export default function Messages({ user }) {
   const [filteredUsers, setFilteredUsers] = useState([])
 
   const getUsers = async () => {
-    const { data } = await axios.get("/api/profiles");
+    const { data } = await axios.get("/api/cache/discover");
+    data = JSON.parse(data)
+    console.log(data)
     console.log(data.filter(a => a.email == "marko@ventnorwebagency.com"))
     setUsers(data.filter(a => a.show_on_discover));
     setFilteredUsers(data.filter(a => a.show_on_discover))
