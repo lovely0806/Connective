bafa1f27c71ba4e2c3525a0279b1e495a826abea
lovely0806 <star.software.guy@gmail.com>
2022-12-31 00:51:54 +0200

    Automatically open latest coversation

--- connective/pages/app/messages.js
+++ connective/pages/app/messages.js
@@ -221,6 +221,21 @@ export default function Messages({ user }) {
   const { newUser } = router.query;
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState();
+
+  // Automatically open latest openned coversation when navigating to messages page
+  useEffect(() => {
+    let x;
+    if(sessionStorage.selectedUser) x= JSON.parse(sessionStorage.selectedUser);
+    if(x !== undefined) {
+      setSelectedUser(x);
+    }
+  }, []);
+  useEffect(() => {
+    if(selectedUser != undefined ) {
+      window.sessionStorage.setItem("selectedUser", JSON.stringify(selectedUser));
+    }
+  }, [selectedUser]);
+
   const [conversations, setConversations] = useState([]);
   const [array1, setArray1] = useState([]);
 
