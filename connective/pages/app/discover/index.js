cf070946cd3842b180a032f64551e31f2b6ee987
lovely0806 <star.software.guy@gmail.com>
2023-01-17 14:13:08 -0500

    First

--- connective/pages/app/discover/index.js
+++ connective/pages/app/discover/index.js
@@ -49,9 +49,9 @@ export default function Messages({ user }) {
 
   const getUsers = async () => {
     let start = Date.now();
-    //const { data } = await Recache.cached(137, axios.get, ["/api/profiles"]);
+    const { data } = await Recache.cached(137, axios.get, ["/api/profiles"]);
     let elapsed = Date.now() - start;
-    const {data} = await axios.get("/api/profiles")
+    //const {data} = await axios.get("/api/profiles")
     setUsers(data.filter((a) => a.show_on_discover));
   };
 
