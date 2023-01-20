2fb579cc28d65f9c869cf66367acf088b269f01b
lovely0806 <star.software.guy@gmail.com>
2023-01-20 23:36:55 +0200

    fix user details layout in messages

--- connective/pages/app/messages.tsx
+++ connective/pages/app/messages.tsx
@@ -286,29 +286,31 @@ const Chat = ({
   );
 };
 
+
+
 const UserDetails = ({ selectedUser }) => {
   return (
-    <div className="w-1/5 h-full px-2">
+    <div className="w-1/5 h-full px-2 py-3">
       {selectedUser && (
         <>
           {selectedUser.logo ? (
             <img
               src={selectedUser.logo}
-              className="w-12 h-12 bg-white rounded-full shadow-lg"
+              className="w-12 h-12 bg-white rounded-full shadow-lg m-auto"
             />
           ) : (
             <Avatar
-              className="rounded-full shadow-lg mt-5"
+              className="rounded-full shadow-lg m-auto"
               width="30px"
               height="30px"
               title={selectedUser.username}
             />
           )}
-          <p className="font-bold text-lg text-center mt-5">
+          <p className="font-bold text-lg text-center mt-2">
             {selectedUser.username}
           </p>
-          <p className="text-sm font-bold mt-10">Contact Details:</p>
-          <div className="flex flex-row gap-2 my-5">
+          <p className="text-sm font-bold mt-4">Contact Details:</p>
+          <div className="flex flex-row gap-2 my-3">
             <div className="bg-black/5 rounded-full p-[7px] w-8 h-8 shadow-lg">
               <img src="/assets/email.png"></img>
             </div>
