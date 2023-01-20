8898e570d05128a9fabbc947d6eb81f4af625b5f
lovely0806 <star.software.guy@gmail.com>
2023-01-20 13:14:26 -0500

    Hotfix

--- connective/services/activity/activityFeed.ts
+++ connective/services/activity/activityFeed.ts
@@ -88,10 +88,4 @@ export namespace ActivityFeed {
       await addActivityFeed("pageview_discover", `user ${userId} viewed the discover page`);
     }
   }
-
-  export class Discover {
-    static async viewDiscover(userId: string) {
-      await addActivityFeed("pageview_discover", `user ${userId} viewed the discover page`);
-    }
-  }
 }
